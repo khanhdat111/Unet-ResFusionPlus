9362741bae42d5f58fc267bb8907895a6d3b5ce6
khanhdat111 <128824280+khanhdat111@users.noreply.github.com>
2024-06-22 12:00:42 +0700

    Update train.py

--- train.py
+++ train.py
@@ -3,8 +3,20 @@ import gc
 from CustomLayers.DataAugmentaion import augment_images
 from ModelArchitecture.DiceLoss import dice_metric_loss
 from ModelArchitecture.Unet_ResFusionPlus import create_model
+from ImageLoader.ImageLoader2D import load_data
 
+#Prepare data:
+train_path = "path_your_train_data"
+val_path = "path_your_valid_data"
+test_path = "path_your_test_data"
 
+img_height, img_width = 352, 352
+
+X_train, Y_train = load_data(img_height, img_width, "train", train_path)
+X_val, Y_val = load_data(img_height, img_width, "validation", val_path)
+X_test, Y_test = load_data(img_height, img_width, "test", test_path)
+
+#Training
 learning_rate = 1e-4
 
 optimizer = tf.keras.optimizers.AdamW(learning_rate=learning_rate, weight_decay= 1e-4)
